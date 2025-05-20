c15a69cc2c4d301843b477a8a5b63e87ecf4baff
eliadom <u1972823@campus.udg.edu>
2025-05-20 21:14:21 +0200

    TestWebCrawlerService create beforeEach setup

--- src/test/java/webcrawler/TestWebCrawlerService.java
+++ src/test/java/webcrawler/TestWebCrawlerService.java
@@ -1,4 +1,20 @@
 package webcrawler;
 
+import org.example.entity.HNEntry;
+import org.example.service.WebCrawlerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 public class TestWebCrawlerService {
+
+    @Mock
+    WebCrawlerService webCrawlerService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        webCrawlerService = Mockito.mock(WebCrawlerService.class);
+    }
 }
