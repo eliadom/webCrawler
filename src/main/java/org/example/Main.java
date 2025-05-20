c9f44f9ab163826ecd22f8d9673a8e404b14cdaa
eliadom <eliadm100@gmail.com>
2025-05-20 15:05:41 +0200

    Chore: add access log functionalities

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.entity.HNEntry;
 import org.example.service.WebCrawlerService;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,20 +17,27 @@ public class Main {
         do {
             webCrawlerService.showCrawlerInstructions();
             inputValue = input.nextLine();
-            switch (inputValue) {
-                case "f":
-                    allEntries = webCrawlerService.getLast30();
-                    webCrawlerService.printEntries(allEntries);
-                    break;
-                case "m":
-                    webCrawlerService.printEntries(webCrawlerService.moreThanFiveByComments(allEntries));
-                    break;
-                case "l":
-                    webCrawlerService.printEntries(webCrawlerService.lessOrEqualThanFiveByPoints(allEntries));
-                    break;
-                case "e":
-                    break;
+            try {
+                switch (inputValue) {
+                    case "f":
+                        allEntries = webCrawlerService.getLast30();
+                        webCrawlerService.printEntries(allEntries);
+                        webCrawlerService.saveAccess();
+                        break;
+                    case "m":
+                        webCrawlerService.printEntries(webCrawlerService.moreThanFiveByComments(allEntries));
+                        webCrawlerService.saveAccess(true);
+                        break;
+                    case "l":
+                        webCrawlerService.printEntries(webCrawlerService.lessOrEqualThanFiveByPoints(allEntries));
+                        webCrawlerService.saveAccess(false);
+                        break;
+                    case "e":
+                        break;
+                }
+            } catch (IOException e) {
+                System.out.println("We could not register this action into the access log.");
             }
-        } while (!inputValue.equals("e"));
+        } while (!inputValue.equalsIgnoreCase("e"));
     }
 }
