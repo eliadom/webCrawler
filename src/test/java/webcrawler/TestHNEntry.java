8ab5f518ac4bfa6e81ce178e892243180b8b29da
eliadom <u1972823@campus.udg.edu>
2025-05-20 23:09:25 +0200

    Refactor HNEntry: Public to private

--- src/test/java/webcrawler/TestHNEntry.java
+++ src/test/java/webcrawler/TestHNEntry.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
-public class TestHNEntry {
+private class TestHNEntry {
 
     HNEntry hnEntry;
 
