54078838b92a29def7c6b8c38f70f567e579d99f
eliadom <eliadm100@gmail.com>
2025-05-20 13:06:31 +0200

    Chore: Implement menu interaction

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,32 @@ import org.example.entity.HNEntry;
 import org.example.service.WebCrawlerService;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         WebCrawlerService webCrawlerService = new WebCrawlerService();
-        webCrawlerService.showCrawlerInstructions();
         List<HNEntry> allEntries = webCrawlerService.getLast30();
         webCrawlerService.printEntries(allEntries);
+        Scanner input = new Scanner(System.in);
+        String inputValue = "";
+        do {
+            webCrawlerService.showCrawlerInstructions();
+            inputValue = input.nextLine();
+            switch (inputValue) {
+                case "f":
+                    allEntries = webCrawlerService.getLast30();
+                    webCrawlerService.printEntries(allEntries);
+                    break;
+                case "m":
+                    webCrawlerService.printEntries(webCrawlerService.moreThanFiveByComments(allEntries));
+                    break;
+                case "l":
+                    webCrawlerService.printEntries(webCrawlerService.lessOrEqualThanFiveByPoints(allEntries));
+                    break;
+                case "e":
+                    break;
+            }
+        } while (!inputValue.equals("e"));
     }
 }
