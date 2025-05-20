392c2586d2614a44c1c08cb60946d983f722d70e
eliadom <eliadm100@gmail.com>
2025-05-20 12:14:00 +0200

    Create getLast30

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.service.WebCrawlerService;
+
 public class Main {
     public static void main(String[] args) {
+        WebCrawlerService webCrawlerService = new WebCrawlerService();
+
+        webCrawlerService.getLast30();
         System.out.println("Hello world!");
     }
 }
--- src/main/java/org/example/service/WebCrawlerService.java
+++ src/main/java/org/example/service/WebCrawlerService.java
@@ -1,4 +1,32 @@
 package org.example.service;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WebCrawlerService {
+
+    public Elements getLast30() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://news.ycombinator.com/news";
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+        String htmlResponse = response.getBody();
+        Document doc = Jsoup.parse(htmlResponse);
+
+        Element titles = doc.getElementById("hnmain");
+        Elements titleElements = titles.getElementsByAttributeValue("border", "0");
+        Element allEntries = titleElements.get(2);
+        Elements allEntriesDivided = allEntries.select("table > tbody > tr");
+        allEntriesDivided = allEntriesDivided.stream().filter(entry -> entry.attributes().size() == 0 || entry.className().equals("athing submission")).collect(Collectors.toCollection(Elements::new));
+        // Divided into number,title; points and number of comments.
+        return allEntriesDivided;
+    }
 }
