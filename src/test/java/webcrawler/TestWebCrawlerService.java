18f028ab1232327e4c602262465fdf4c548cdcc0
eliadom <u1972823@campus.udg.edu>
2025-05-20 21:28:42 +0200

    Refactor TestWebCrawlerService saveAccess fetch

--- src/test/java/webcrawler/TestWebCrawlerService.java
+++ src/test/java/webcrawler/TestWebCrawlerService.java
@@ -2,9 +2,13 @@ package webcrawler;
 
 import org.example.service.WebCrawlerService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class TestWebCrawlerService {
 
@@ -15,5 +19,11 @@ public class TestWebCrawlerService {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
         webCrawlerService = Mockito.mock(WebCrawlerService.class);
+    }
+
+    @Test
+    public void testWebCrawlerServiceSaveAccessFetchAllOk() {
+        // Throw can only be caused to internal error. Not client cases. Just controlling the error
+        assertDoesNotThrow(() -> webCrawlerService.saveAccess());
     }
 }
