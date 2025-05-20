23d91c58141483496b910fd84ad15eb8c983747a
eliadom <u1972823@campus.udg.edu>
2025-05-20 20:46:30 +0200

    TestHNEntry mock with Mockito

--- src/test/java/webcrawler/TestHNEntry.java
+++ src/test/java/webcrawler/TestHNEntry.java
@@ -4,17 +4,27 @@ import org.example.entity.HNEntry;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.mockito.Mockito.*;
 
 public class TestHNEntry {
+
     HNEntry hnEntry;
+
+    @Mock
+    HNEntry mockHNEntry;
 
     @BeforeEach
     public void setUp() {
+        MockitoAnnotations.openMocks(this);
         hnEntry = new HNEntry(123, "This is - My Post", 54, 893);
+        mockHNEntry = Mockito.mock(HNEntry.class);
+
     }
 
     @Test
@@ -31,4 +41,5 @@ public class TestHNEntry {
     public void testGetPoints() {
         assertEquals(hnEntry.getPoints(),54);
     }
+
 }
